275fb4b046fa2a92ec2778d5f2410e336328776a
Nexxds <42595522+Nexxds@users.noreply.github.com>
2018-12-17 09:44:55 -0200

    Create ItensNobiliarquicos.js

---
+++ ItensNobiliarquicos.js
@@ -0,0 +1,160 @@
+ $(function() {
+  	$('#navegar1').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo1').css('display','block');
+  	});
+  
+  	$('#navegar2').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo2').css('display','block');
+  	});
+  
+    	$('#navegar3').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo3').css('display','block');
+  	});
+  
+    	$('#navegar4').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo4').css('display','block');
+  	});
+  
+   	$('#navegar5').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo5').css('display','block');
+  
+  	});
+  
+  	$('#navegar6').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo6').css('display','block');
+  	});
+  
+  	$('#navegar7').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo7').css('display','block');
+  	});
+  
+    	$('#navegar8').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo8').css('display','block');
+  	});
+    	$('#navegar9').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo9').css('display','block');
+  	});
+    	$('#navegar10').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo10').css('display','block');
+  	});
+    	$('#navegar11').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo11').css('display','block');
+  	});
+    	$('#navegar12').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo12').css('display','block');
+  	});
+    	$('#navegar13').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo13').css('display','block');
+  	});
+    	$('#navegar14').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo14').css('display','block');
+  	});
+      	$('#navegar15').click(function() {
+  		$('.navegar a').each(function() {
+  			$(this).removeClass('clicadaum');
+  		});
+  		$('.ladodonavegar div.Titulo').each(function() {
+  			$(this).css('display','none');
+  		});
+  		$(this).addClass('clicadaum');
+  		$('.Titulo15').css('display','block');
+  	});
+  });
